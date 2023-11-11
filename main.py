2dde77be3358f7e1ce16d37707c8f2ab0245730d
vprivalova <privalovaa.vv@gmail.com>
2023-11-12 02:09:20 +0700

    internal commit 3

--- main.py
+++ main.py
@@ -6,20 +6,20 @@ all_words = []
 
 with open('input_txt', 'r', encoding='utf-8') as file1:
     number = file1.readline()
+
     for paragraph in file1:
         words = paragraph.split()
+
         for word in words:
             if word == '-' or word == '–':
                 words.remove(word)
+
         all_words.extend(words)
 
 for word in all_words:
-    if word in unique_words:
-        continue
-    else:
+    if word not in unique_words:
         unique_words.append(word)
 
-list_words = {}
 following_words = []
 second_list = []
 amount = 0
@@ -30,9 +30,11 @@ for elem in unique_words:
 
     for i in range(amount):
         index = all_words_2.index(elem)
+
         if index != len(all_words) - 1:
             following_words.append(all_words_2[index+1])
             all_words_2[index] = all_words_2[index].replace(all_words_2[index], '+')
+
     following_words_2 = following_words.copy()
     second_list.append(following_words_2)
     following_words.clear()
@@ -42,6 +44,7 @@ bred_line = ''
 
 for j in range(int(number)):
     beg = random.randint(0, len(unique_words) - 1)
+
     while unique_words[beg][0].isupper() is False:
         beg = random.randint(0, len(unique_words) - 1)
 
@@ -54,6 +57,7 @@ for j in range(int(number)):
         bred_line = bred_line + ' ' + second_list[index][end]
         index = unique_words.index(second_list[index][end])
         end = random.randint(0, len(second_list[index]) - 1)
+
     bred_line = bred_line + ' ' + second_list[index][end]
     bred.append(bred_line)
     bred_line = ''
